0c8ff3242a511e849567c8beb5b6f12f97741466
Sorahawk <63446212+Sorahawk@users.noreply.github.com>
2021-05-27 11:46:35 +0800

    Update Simple PDF Text Extractor.py

--- Simple PDF Text Extractor.py
+++ Simple PDF Text Extractor.py
@@ -8,11 +8,10 @@ outputPath = mainPath + '/Output'
 os.chdir(mainPath + '/Input')
 
 for infile in glob.glob('*.pdf'):	
-	fullText = []
-
 	pdfFile = open(infile, 'rb')
 	reader = PyPDF4.PdfFileReader(pdfFile)
 
+	fullText = []
 	for pageNum in range(reader.numPages):
 		page = reader.getPage(pageNum)
 		fullText.append(page.extractText())
@@ -20,7 +19,6 @@ for infile in glob.glob('*.pdf'):
 	pdfFile.close()
 
 	outfileName = "{}/{}txt".format(outputPath, infile[:-3])
-
 	with open(outfileName, 'w') as outfile:
 		for line in fullText:
 			outfile.write(line)
